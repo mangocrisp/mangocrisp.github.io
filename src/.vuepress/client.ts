366b59997235b1d61b19cf27ad64996423ef5516
yinxijie <yinxijie@gzgxinfo.com>
2024-09-11 03:34:19 +0800

    图标显示出来了

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,16 +1,16 @@
 // 为项目主页的特性添加闪光效
 import "vuepress-theme-hope/presets/shinning-feature-panel.scss";
-// 将博主信息移动至文章列表的左侧
+// // 将博主信息移动至文章列表的左侧
 import "vuepress-theme-hope/presets/left-blog-info.scss";
-// 将博主头像裁剪为圆形
+// // 将博主头像裁剪为圆形
 import "vuepress-theme-hope/presets/round-blogger-avatar.scss";
-// 为页面图标添加鼠标悬停的跳动效果
+// // 为页面图标添加鼠标悬停的跳动效果
 import "vuepress-theme-hope/presets/bounce-icon.scss";
-// 隐藏导航栏图标
-import "vuepress-theme-hope/presets/hide-navbar-icon.scss";
-// 隐藏侧边栏图标
-import "vuepress-theme-hope/presets/hide-sidebar-icon.scss";
-// 为所有 hr 元素添加驾驶的车图标
+// // 隐藏导航栏图标
+// import "vuepress-theme-hope/presets/hide-navbar-icon.scss";
+// // 隐藏侧边栏图标
+// import "vuepress-theme-hope/presets/hide-sidebar-icon.scss";
+// // 为所有 hr 元素添加驾驶的车图标
 import "vuepress-theme-hope/presets/hr-driving-car.scss";
 import { defineClientConfig } from "vuepress/client";
 import { setupTransparentNavbar } from "vuepress-theme-hope/presets/transparentNavbar.js";
