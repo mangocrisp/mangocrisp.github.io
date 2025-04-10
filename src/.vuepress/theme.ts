abb2be8ceafab658e9ada9e02020979ceeca2c89
Mango Crisp <15014633363@163.com>
2025-04-10 17:46:26 +0800

    多数据源动态判断

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,157 +3,93 @@ import { git, hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
-export default hopeTheme({
-  hostname: "https://mangocrisp.github.io",
-  // 作者信息
-  author: {
-    name: "Mango Crisp",
-    url: "https://github.com/mangocrisp",
-  },
-  // 许可
-  license: "Apache 2.0",
-  // 网站的 icon
-  favicon: "/logo.svg",
-  // 图标库
-  iconAssets: "iconify",
-  // 前缀
-  iconPrefix: "",
-  darkmode: "switch",
-  // 导航栏的图标
-  logo: "/logo.svg",
-  // 仓库地址
-  repo: "https://github.com/mangocrisp/mangocrisp.github.io",
-  // 文档仓库地址
-  docsRepo: "https://github.com/mangocrisp/mangocrisp.github.io",
-  // 文档存放的分支
-  docsBranch: "main",
-  // 仓库地址图片，可以自动识别 git gitee gitlab 等
-  //repoLabel: "GigHub",
-  docsDir: "src",
-  // 导航栏
-  navbar,
-  // 导航栏布局
-  navbarLayout: {
-    start: ["Brand"],
-    center: ["Links"],
-    end: ["Search", "Language", "Outlook", "Repo"],
-  },
-  // 全屏按钮
-  fullscreen: true,
-  // 侧边栏
-  sidebar,
-  sidebarSorter:['readme', 'order', 'title', 'filename'],
-  // 侧边栏嵌套的标题深度
-  headerDepth: 2,
-  // 页脚
-  footer: "Mango Crisp 的小破站",
-  // 版本信息，不写会根据作者信息和许可自动生成
-  //copyright: "",
-  // 是否显示页脚
-  displayFooter: true,
-
-  // 博客相关
-  blog: {
-    description: "Know why,so you can know how.",
-    intro: "/intro.html",
-    avatar: "https://avatars.githubusercontent.com/u/46065167?v=4",
-    sidebarDisplay: "mobile",
-    medias: {
-      GitHub: "https://github.com/mangocrisp",
-      GitLab: "https://jihulab.com/mangocrisp",
-      Gitee: "https://gitee.com/mangocrisp",
-      BiliBili: "https://space.bilibili.com/25505115",
-      "163Music": "https://music.163.com/#/user/home?id=528192925",
-      // QQ 推广工具还需要申请公司啥的，比较麻烦就没搞
-      //QQ: "https://admin.qidian.qq.com/static_proxy/b2b-qq/wpa-link/index.html#/person?uin=2415446179",
-      Qzone: "https://user.qzone.qq.com/2415446179",
-      "QQMusic": "https://y.qq.com/n/ryqq/profile/like/song?uin=owv57KvP7w6lNv",
-      Steam: "https://steamcommunity.com/profiles/76561198867566298",
+export default hopeTheme(
+  {
+    hostname: "https://mangocrisp.github.io",
+    // 作者信息
+    author: {
+      name: "Mango Crisp",
+      url: "https://github.com/mangocrisp",
     },
-  },
-
-  // 加密配置
-  encrypt: {
-    config: {
-      //"/demo/encrypt.html": ["1234"],
+    // 许可
+    license: "Apache 2.0",
+    // 网站的 icon
+    favicon: "/logo.svg",
+    darkmode: "switch",
+    // 导航栏的图标
+    logo: "/logo.svg",
+    // 仓库地址
+    repo: "https://github.com/mangocrisp/mangocrisp.github.io",
+    // 文档仓库地址
+    docsRepo: "https://github.com/mangocrisp/mangocrisp.github.io",
+    // 文档存放的分支
+    docsBranch: "main",
+    // 仓库地址图片，可以自动识别 git gitee gitlab 等
+    //repoLabel: "GigHub",
+    docsDir: "src",
+    // 导航栏
+    navbar,
+    // 导航栏布局
+    navbarLayout: {
+      start: ["Brand"],
+      center: ["Links"],
+      end: ["Search", "Language", "Outlook", "Repo"],
     },
-  },
+    // 全屏按钮
+    fullscreen: true,
+    // 侧边栏
+    sidebar,
+    sidebarSorter: ["readme", "order", "title", "filename"],
+    // 侧边栏嵌套的标题深度
+    //headerDepth: 2,
+    // 页脚
+    footer: "Mango Crisp 的小破站",
+    // 版本信息，不写会根据作者信息和许可自动生成
+    //copyright: "",
+    // 是否显示页脚
+    displayFooter: true,
 
-  // 多语言配置
-  metaLocales: {
-    editLink: "在 GitHub 上编辑此页",
-  },
-
-  // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
-  // hotReload: true,
-
-  // 在这里配置主题提供的插件
-  plugins: {
-    // 水印
-    //watermark: true,
-    // 版本信息
-    copyright: true,
-    // copyright: { 
-    //   global: true,
-    //   author: "Mango Crisp",
-    //   maxLength: 100,
-    //   disableCopy: false
-    //  },
-    // 图片浏览器
-    photoSwipe: true,
-    // 公告
-    notice: [
-      {
-        path: "/",
-        title: "如果你有问题",
-        content: "请联系我！",
-        actions: [
-          {
-            text: "让我看看你",
-            link: "/intro.html",
-            type: "primary",
-          },
-          { text: "不认识" },
-        ],
-      }],
-    // 博客
-    blog: true,
-    // 搜索
-    //searchPro: true,
-    searchPro: {
-      // 是否索引内容
-      indexContent: true,
-      // 是否自动提示搜索建议
-      autoSuggestions: true,
-      // 存储搜索查询词历史的最大数量
-      queryHistoryCount: 5,
-      // 存储搜索结果历史的最大数量
-      resultHistoryCount: 5
-    },
-    // 评论
-    comment: {
-      provider: "Giscus",
-      repo: "mangocrisp/mangocrisp.github.io",
-      repoId: "R_kgDOMoyjDg",
-      category: "General",
-      categoryId: "DIC_kwDOMoyjDs4CiTlw",
-      mapping: "pathname",
-      // 模板要用这个：https://github.com/walinejs/waline/tree/main/example
-      //provider: "Waline",
-      // 服务地址是你的应用名 https://waline-d.vercel.app/
-      //serverURL: "https://waline-d.vercel.app/"
+    // 博客相关
+    blog: {
+      description: "Know why,so you can know how.",
+      intro: "/intro.html",
+      avatar: "https://avatars.githubusercontent.com/u/46065167?v=4",
+      sidebarDisplay: "mobile",
+      medias: {
+        GitHub: "https://github.com/mangocrisp",
+        GitLab: "https://jihulab.com/mangocrisp",
+        Gitee: "https://gitee.com/mangocrisp",
+        BiliBili: "https://space.bilibili.com/25505115",
+        "163Music": "https://music.163.com/#/user/home?id=528192925",
+        // QQ 推广工具还需要申请公司啥的，比较麻烦就没搞
+        //QQ: "https://admin.qidian.qq.com/static_proxy/b2b-qq/wpa-link/index.html#/person?uin=2415446179",
+        Qzone: "https://user.qzone.qq.com/2415446179",
+        QQMusic: "https://y.qq.com/n/ryqq/profile/like/song?uin=owv57KvP7w6lNv",
+        Steam: "https://steamcommunity.com/profiles/76561198867566298",
+      },
     },
 
-    components: {
-      components: ["Badge", "VPBanner", "Share", "VPCard"],
+    // 加密配置
+    encrypt: {
+      config: {
+        //"/demo/encrypt.html": ["1234"],
+      },
     },
+
+    // 多语言配置
+    metaLocales: {
+      editLink: "在 GitHub 上编辑此页",
+    },
+
+    // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
+    // hotReload: true,
 
     // 此处开启了很多功能用于演示，你应仅保留用到的功能。
-    mdEnhance: {
+    markdown: {
       align: true,
       attrs: true,
       breaks: true,
-      codetabs: true,
+      // codetabs: true,
       component: true,
       demo: true,
       figure: true,
@@ -227,64 +163,133 @@ export default hopeTheme({
       // install sandpack-vue3 before enabling it
       // sandpack: true,
     },
+    // 在这里配置主题提供的插件
+    plugins: {
+      // 图标库
+      icon: {
+        assets: "iconify",
+        // 前缀
+        prefix: "",
+      },
+      // 水印
+      //watermark: true,
+      // 版本信息
+      copyright: true,
+      // copyright: {
+      //   global: true,
+      //   author: "Mango Crisp",
+      //   maxLength: 100,
+      //   disableCopy: false
+      //  },
+      // 图片浏览器
+      photoSwipe: true,
+      // 公告
+      notice: [
+        {
+          path: "/",
+          title: "如果你有问题",
+          content: "请联系我！",
+          actions: [
+            {
+              text: "让我看看你",
+              link: "/intro.html",
+              type: "primary",
+            },
+            { text: "不认识" },
+          ],
+        },
+      ],
+      // 博客
+      blog: true,
+      // 搜索
+      //searchPro: true,
+      slimsearch: {
+        // 是否索引内容
+        indexContent: true,
+        // 是否自动提示搜索建议
+        autoSuggestions: true,
+        // 存储搜索查询词历史的最大数量
+        queryHistoryCount: 5,
+        // 存储搜索结果历史的最大数量
+        resultHistoryCount: 5,
+      },
+      // 评论
+      comment: {
+        provider: "Giscus",
+        repo: "mangocrisp/mangocrisp.github.io",
+        repoId: "R_kgDOMoyjDg",
+        category: "General",
+        categoryId: "DIC_kwDOMoyjDs4CiTlw",
+        mapping: "pathname",
+        // 模板要用这个：https://github.com/walinejs/waline/tree/main/example
+        //provider: "Waline",
+        // 服务地址是你的应用名 https://waline-d.vercel.app/
+        //serverURL: "https://waline-d.vercel.app/"
+      },
 
-    // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cacheImage: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
+      components: {
+        components: ["Badge", "VPBanner", "Share", "VPCard"],
+      },
+
+      // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
+      // pwa: {
+      //   favicon: "/favicon.ico",
+      //   cacheHTML: true,
+      //   cacheImage: true,
+      //   appendBase: true,
+      //   apple: {
+      //     icon: "/assets/icon/apple-icon-152.png",
+      //     statusBarColor: "black",
+      //   },
+      //   msTile: {
+      //     image: "/assets/icon/ms-icon-144.png",
+      //     color: "#ffffff",
+      //   },
+      //   manifest: {
+      //     icons: [
+      //       {
+      //         src: "/assets/icon/chrome-mask-512.png",
+      //         sizes: "512x512",
+      //         purpose: "maskable",
+      //         type: "image/png",
+      //       },
+      //       {
+      //         src: "/assets/icon/chrome-mask-192.png",
+      //         sizes: "192x192",
+      //         purpose: "maskable",
+      //         type: "image/png",
+      //       },
+      //       {
+      //         src: "/assets/icon/chrome-512.png",
+      //         sizes: "512x512",
+      //         type: "image/png",
+      //       },
+      //       {
+      //         src: "/assets/icon/chrome-192.png",
+      //         sizes: "192x192",
+      //         type: "image/png",
+      //       },
+      //     ],
+      //     shortcuts: [
+      //       {
+      //         name: "Demo",
+      //         short_name: "Demo",
+      //         url: "/demo/",
+      //         icons: [
+      //           {
+      //             src: "/assets/icon/guide-maskable.png",
+      //             sizes: "192x192",
+      //             purpose: "maskable",
+      //             type: "image/png",
+      //           },
+      //         ],
+      //       },
+      //     ],
+      //   },
+      // },
+    },
   },
-}, {
-  custom: true
-});
+  {
+    custom: true,
+  }
+);
